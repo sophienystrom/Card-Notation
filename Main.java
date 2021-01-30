ec734b5df7e1ec6595a3f36cd4ea2a9c6b1723a8
snystrom <sophiekarennystrom@gmail.com>
2021-01-30 18:50:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+  
+
+ Scanner userInput = new Scanner(System.in);
+ System.out.print("Type card notation: ");
+ String inputNotation = userInput.nextLine();
+
+ switch (inputNotation) {
+
+ case (input.Notation.substring(0,1).equals("Q") || inputNotation.substring(0,1).equals("q")):
+ System.out.println("hi");
+ break;
+ default: 
+ System.out.println("bye");
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
