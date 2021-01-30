99d8a9f0f3dcd872f59df78f31e42db415817e16
snystrom <sophiekarennystrom@gmail.com>
2021-01-30 19:14:03 +0000

    Wrote out two switch statements that are specific to either the first character or second character of the card notation input.

--- Main.java
+++ Main.java
@@ -8,17 +8,81 @@ class Main {
  System.out.print("Type card notation: ");
  String inputNotation = userInput.nextLine();
 
- switch (inputNotation) {
+ String cardValue = inputNotation.substring(0,1);
+ String cardSymbol = inputNotation.substring(1,2);
 
- case (input.Notation.substring(0,1).equals("Q") || inputNotation.substring(0,1).equals("q")):
- System.out.println("hi");
+ switch (cardValue) {
+ case "2":
+ cardValue = "Two";
  break;
- default: 
- System.out.println("bye");
-
+ case "3":
+ cardValue = "Three";
+ break;
+ case "4":
+ cardValue = "Four";
+ break;
+ case "5":
+ cardValue = "Five";
+ break;
+ case "6":
+ cardValue = "Six";
+ break;
+ case "7":
+ cardValue = "Seven";
+ break;
+ case "8":
+ cardValue = "Eight";
+ break;
+ case "9":
+ cardValue = "Nine";
+ break;
+ case "10":
+ cardValue = "Ten";
+ break;
+ case "Q":
+ cardValue = "Queen";
+ break;
+ case "A":
+ cardValue = "Ace";
+ break;
+ case "K":
+ cardValue = "King";
+ break;
+ case "J":
+ cardValue = "Jack";
+ break;
+ default:
+ cardValue = "invalid";
+ System.out.println("invalid card value entry");
  }
 
+ switch (cardSymbol) {
+ case "S":
+ cardSymbol = "Spades";
+ break;
+ case "C":
+ cardSymbol = "Clubs";
+ break;
+ case "H":
+ cardSymbol = "Hearts";
+ break;
+ case "D":
+ cardSymbol = "Diamonds";
+ break;
+ default:
+ cardSymbol = "invalid";
+ System.out.println("invalid card symbol entry");
+ }
 
+ if (cardSymbol.equals("invalid") || cardValue.equals("invalid") )
+ {
+  System.out.println("Full card description cannot be given due to invalid input");
+ }
+ else 
+ {
+ System.out.println(cardValue + " of " + cardSymbol);
+ }
+ 
 
 
 
